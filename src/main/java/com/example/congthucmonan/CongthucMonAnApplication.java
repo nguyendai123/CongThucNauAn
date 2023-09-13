d28f3915a5cb4add38a06ab189ee872e0105969c
nguyendai123 <nguyenvandai1232001@gmail.com>
2023-09-13 10:01:13 +0700

    first commit

---
+++ src/main/java/com/example/congthucmonan/CongthucMonAnApplication.java
@@ -0,0 +1,13 @@
+package com.example.congthucmonan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongthucMonAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongthucMonAnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/congthucmonan/CongthucMonAnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.congthucmonan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongthucMonAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
